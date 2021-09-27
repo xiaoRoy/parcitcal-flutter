f23714b97c85bc38ad974a035bc4228b26e38f07
Jerry Li <jerli@xogrp.com>
2021-09-27 23:54:45 +0800

    Learned BoxDecoration.

--- chap1/hello_world/lib/widget_of_the_week/showcase.dart
+++ chap1/hello_world/lib/widget_of_the_week/showcase.dart
@@ -7,7 +7,7 @@ class WidgetOfTheWeekShowcase extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('widget of the week')),
-      body: NormalContainer(),
+      body: ContainerColumn(),
     );
   }
 }
