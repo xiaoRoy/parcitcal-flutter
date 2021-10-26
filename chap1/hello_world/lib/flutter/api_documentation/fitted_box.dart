4668763c8115dfe2e68cd267e3377f108955a42c
Jerry Li <jerli@xogrp.com>
2021-10-26 23:49:19 +0800

    Setup FittedBoxDemonstration.

---
+++ chap1/hello_world/lib/flutter/api_documentation/fitted_box.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class FittedBoxDemonstration extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+
+class _FittedBoxDemonstrationState extends State<FittedBoxDemonstration> {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
