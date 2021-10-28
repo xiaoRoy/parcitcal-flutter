188e594a0fa9e66d26de1d5dedf176b9c83562c6
Jerry Li <jerli@xogrp.com>
2021-10-28 23:48:20 +0800

    Learned box constratints froces an infinite height.

--- chap1/hello_world/lib/flutter/api_documentation/sized_box.dart
+++ chap1/hello_world/lib/flutter/api_documentation/sized_box.dart
@@ -1,18 +1,57 @@
-
-
 import 'package:flutter/material.dart';
 
 class SizedBoxDocumentation extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    
-    return SizedBox(
-      width: 400,
-      height: 400,
-      child: Container(
-        color: Colors.red,
-      ),
+    return _buildColumnWithInfiniteChild();
+  }
+
+  Widget _budldFlexibleColumn() {
+    return Column(
+      children: [
+        Flexible(
+          child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: Container(
+              color: Colors.blueGrey,
+            ),
+          ),
+        ),
+        Container(
+          color: Colors.limeAccent,
+          height: 200,
+        ),
+      ],
     );
+  }
+
+  Widget _buildEmptyColumn() {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: double.infinity, // why??
+          child: Container(
+            color: Colors.blueGrey,
+          ),
+        ),
+        Container(
+          color: Colors.limeAccent,
+          height: 200,
+        )
+      ],
+    );
+  }
+
+  //BoxConstraints forces an infinite height.
+  Widget _buildColumnWithInfiniteChild() {
+    return Column(children: [
+      Container(
+        color: Colors.cyan,
+        height: double.infinity,
+        width: 300,
+      ),
+    ],);
   }
 }
